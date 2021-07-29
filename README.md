8dca333ce49ff12481f40e63eec7c4181b61230f
TranTienDe <sakurahonni@gmail.com>
2021-07-30 00:04:19 +0700

    setup Primeng

--- README.md
+++ README.md
@@ -25,3 +25,9 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+#PrimeNg
+https://primefaces.org/primeng/showcase/#/
+
+#4001 - Angular 12 - Prime Ng - Notes App - Phân tích + viết đơn giản
+https://www.youtube.com/watch?v=4wwqCIrUork
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { MenubarModule } from 'primeng/menubar';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    CardModule,
+    MenubarModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
